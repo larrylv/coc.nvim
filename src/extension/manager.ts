4228dea80f003ea39ebd1f4a87052eae23d03308
Larry Lv <larrylv@stripe.com>
2023-06-20 11:58:29 -0700

    Increase extension activation timeout to 10s

--- src/extension/manager.ts
+++ src/extension/manager.ts
@@ -442,7 +442,7 @@ export class ExtensionManager {
     let exports: any
     let ext: ExtensionExport
     let subscriptions: Disposable[] = []
-    const timing = createTiming(`activate ${id}`, 5000)
+    const timing = createTiming(`activate ${id}`, 10000)
     let extension: Extension<API> = {
       activate: (): Promise<API> => {
         if (result) return result
